163de9e97ad18642c3de8d9bd8f045769b76930b
kalimruslan <kalimruslan.elabuga@gmail.com>
2019-12-25 11:12:23 +0300

    test

--- app/src/main/java/ru/ruslan/weighttracker/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/ruslan/weighttracker/ui/profile/ProfileActivity.kt
@@ -25,6 +25,9 @@ class ProfileActivity : DaggerAppCompatActivity() {
     }
 
     private fun observerLiveData() {
+        // test
         profileViewModel?.generateAndSetProfileData()
     }
+
+
 }
